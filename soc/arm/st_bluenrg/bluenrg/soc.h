0ec386e2cc29f10a9d1a0469ac854d844e25bcc6
Killian Leray <killian.leray@st.com>
2022-12-19 14:35:48 +0100

    Initial commit
    Adding a test file for commit test
    
    Add bluenrglp soc folder and soc files copied from bluenrg2 github repo (changes to be done)
    
    Update Kconfig.soc for LP
    
    Update few BlueNRG2 SoC files for LP
    
    Moving bluenrg_lp.dtsi file from modules/hal to zephyr/dts
    
    Delete one folder stage because no more series files. Modify some of these files
    
    Add #include .../linker.ld
    
    Rename bluenrg_232 files and configure LEDs and PBs IOs in .dts file
    
    Few files modifications
    
    Boards and soc files modification
    
    Compilation Hello_world vide fonctionnelle
    
    Hello_world build fine and west flash seems fine also
    
    First commit on uart driver implementation for hello_world sample over BlueNRG-LP
    
    Real hello_world sample is compiling and flashing on LP but clocks needs to be managed to make it work
    
    Saved changes before checkout on main branch to play with zephyr on wb55
    
    #include drivers issues in soc.c resolved
    
    Compilation ok with SystemInit SDK fonction added in soc.c file but it's not flashing Error connecting DP : cannot read IDR
    
    west flash almost working for blinky in soc.c
    
    West flash is working with blinky implementation in soc.c
    
    USART and Toggle Led working with everything done in soc.c
    
    Build and flash ok but only *** Booting printed in the console
    
    Save changes before checkout on main branch
    
    soc.c minimum configuration to be able to print from banner.c
    
    BlueNRG-LP reset controller implementation
    
    west manifest update bluenrg_hal
    
    Remove useless files

---
+++ soc/arm/st_bluenrg/bluenrg/soc.h
@@ -0,0 +1,69 @@
+// /*
+//  * Copyright (c) 2016 Open-RnD Sp. z o.o.
+//  * Copyright (c) 2016 BayLibre, SAS
+//  *
+//  * SPDX-License-Identifier: Apache-2.0
+//  */
+
+// /**
+//  * @file SoC configuration macros for the STM32L4 family processors.
+//  *
+//  * Based on reference manual:
+//  *   STM32L4x1, STM32L4x2, STM32L431xx STM32L443xx STM32L433xx, STM32L4x5,
+//  *   STM32l4x6 advanced ARM(r)-based 32-bit MCUs
+//  *
+//  * Chapter 2.2.2: Memory map and register boundary addresses
+//  */
+
+#ifndef _BLUENRG_LP_SOC_H_
+#define _BLUENRG_LP_SOC_H_
+
+
+//#include <sys/util.h>
+
+// #ifndef _ASMLANGUAGE
+
+#include <autoconf.h>
+#include <BlueNRG_LP.h>
+
+/* Add include for DTS generated information */
+//#include <devicetree.h>
+#include <rf_driver_ll_rcc.h>
+#include <rf_driver_ll_pwr.h>
+#include <rf_driver_ll_flash.h>
+#include <rf_driver_ll_gpio.h>
+#include <rf_driver_ll_usart.h>
+#include <rf_driver_ll_utils.h>
+#include <rf_driver_ll_bus.h>
+#include <rf_driver_hal_gpio.h>
+#include <rf_driver_hal_uart.h>
+#include <system_BlueNRG_LP.h>
+//#include <misc.h>
+#endif /* _BLUENRG_LP_SOC_H_ */
+
+
+/*
+ * Copyright (c) 2018 Endre Karlson <endre.karlson@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32L0 family processors.
+ *
+ * Based on reference manual:
+ *   STM32L0X advanced ARM ® -based 32-bit MCUs
+ *
+ * Chapter 2.2: Memory organization
+ */
+
+
+// #ifndef _BLUENRG_LP_SOC_H_
+// #define _BLUENRG_LP_SOC_H_
+
+// #ifndef _ASMLANGUAGE
+
+
+// #endif /* !_ASMLANGUAGE */
+
+// #endif /* _BLUENRG_LP_SOC_H_ */
