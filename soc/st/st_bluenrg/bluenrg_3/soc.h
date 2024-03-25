1289104b0f5912082ac05c76fd0796bc28a189dc
Ali Hozhabri <ali.hozhabri@st.com>
2024-03-25 11:11:13 +0100

    soc: st: st_bluenrg: Add SOC support for BlueNRG-LPS
    Add SOC support for BlueNRG-LPS.
    
    Cleanup the driver.
    
    Signed-off-by: Ali Hozhabri <ali.hozhabri@st.com>

--- soc/st/st_bluenrg/bluenrg_3/soc.h
+++ soc/st/st_bluenrg/bluenrg_3/soc.h
@@ -5,59 +5,34 @@
 //  * SPDX-License-Identifier: Apache-2.0
 //  */
 
-// /**
-//  * @file SoC configuration macros for the STM32L4 family processors.
-//  *
-//  * Based on reference manual:
-//  *   STM32L4x1, STM32L4x2, STM32L431xx STM32L443xx STM32L433xx, STM32L4x5,
-//  *   STM32l4x6 advanced ARM(r)-based 32-bit MCUs
-//  *
-//  * Chapter 2.2.2: Memory map and register boundary addresses
-//  */
+/**
+ * @file SoC configuration macros for the STM32L4 family processors.
+ *
+ * Based on reference manual:
+ *   STM32L4x1, STM32L4x2, STM32L431xx STM32L443xx STM32L433xx, STM32L4x5,
+ *   STM32l4x6 advanced ARM(r)-based 32-bit MCUs
+ *
+ * Chapter 2.2.2: Memory map and register boundary addresses
+ */
 
 #ifndef _BLUENRG_LP_SOC_H_
 #define _BLUENRG_LP_SOC_H_
 
-
-//#include <sys/util.h>
-
-// #ifndef _ASMLANGUAGE
-
 #include <autoconf.h>
+
+#if defined(CONFIG_SOC_BLUENRG_LP)
 #include <BlueNRG_LP.h>
 
+#elif defined(CONFIG_SOC_BLUENRG_LPS)
+#include <BlueNRG_LPS.h>
+
+#else
+	#error "Board not supported"
+#endif /* defined(CONFIG_SOC_BLUENRG_LP) */
+
 /* Add include for DTS generated information */
-//#include <devicetree.h>
 #include <rf_driver_ll_flash.h>
 #include <rf_driver_ll_rcc.h>
 #include <rf_driver_ll_pwr.h>
 #include <rf_driver_ll_system.h>
-//#include <misc.h>
 #endif /* _BLUENRG_LP_SOC_H_ */
-
-
-/*
- * Copyright (c) 2018 Endre Karlson <endre.karlson@gmail.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * @file SoC configuration macros for the STM32L0 family processors.
- *
- * Based on reference manual:
- *   STM32L0X advanced ARM ® -based 32-bit MCUs
- *
- * Chapter 2.2: Memory organization
- */
-
-
-// #ifndef _BLUENRG_LP_SOC_H_
-// #define _BLUENRG_LP_SOC_H_
-
-// #ifndef _ASMLANGUAGE
-
-
-// #endif /* !_ASMLANGUAGE */
-
-// #endif /* _BLUENRG_LP_SOC_H_ */
