029b6e1b85872622a507050a3e49229fb933b622
Michele Sardo <msmttchr@gmail.com>
2026-01-14 22:03:55 +0100

    Add automatic documentation

--- rcp_ble_ot/docs/src/conf.py
+++ rcp_ble_ot/docs/src/conf.py
@@ -25,10 +25,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'bizstyle'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_theme_options = {
     # Disable showing the sidebar. Defaults to 'false'
     'nosidebar': True,
 }
 html_permalinks = False
 html_use_index = False
+# This will add "Last updated on: Jan 14, 2026" (or current date) to the footer
+# html_last_updated_fmt = '%b %d, %Y'
+
+# Optional: If you want to show the specific time as well:
+html_last_updated_fmt = '%Y-%m-%d %H:%M:%S'
